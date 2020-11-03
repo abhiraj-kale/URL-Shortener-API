e7b466058c39b1eaa3728e87dff3e08c3aa6b8d4
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-11-03 23:34:04 +0530

    update

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 var mysql = require('mysql');
 const { url } = require('inspector');
 
+function handleDisconnect() { // Recreate the connection, since the old one cannot be reused.
 
 var con = mysql.createConnection({
   host: "us-cdbr-east-02.cleardb.com",
@@ -14,10 +15,24 @@ var con = mysql.createConnection({
   database: "heroku_5c062220028c37e"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to MySql!");
-});
+con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
